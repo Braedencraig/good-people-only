c02936d7525c15bc6758cc68393320a096e55551
Braeden Craig <braedencraig@gmail.com>
2022-02-28 14:56:11 -0500

    adjust env

--- components/Burger/Burger.js
+++ components/Burger/Burger.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { bool, func } from 'prop-types';
+import { bool, func } from "prop-types";
 import { StyledBurger } from "./Burger.styled";
 
 const Burger = ({ open, setOpen }) => {
   return (
-    <StyledBurger open={open} onClick={() => setOpen(!open)}>
+    <StyledBurger name="button" open={open} onClick={() => setOpen(!open)}>
       <div />
       <div />
       <div />
