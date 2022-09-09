cbe425a1a89e8d3bc16cda0b907eb1f105b4e500
Braeden Craig <braeden.craig@gmail.com>
2022-09-08 23:30:05 -0400

    update

---
+++ pages/careers.js
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+
+export default function Careers() {
+  useEffect(() => {
+    window.location.href =
+      "https://ca.indeed.com/viewjob?cmp=Good-People-Only&t=Label&jk=2a4c218bcfab483a&q=Record+Label+Manager&vjs=3";
+  });
+  return true;
+}
