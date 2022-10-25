f3b4ac857e9d6b7da486785ac658f173837dce84
Braeden Craig <braeden.craig@gmail.com>
2022-10-24 23:33:51 -0400

    new urls

--- pages/careers.js
+++ pages/careers.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export default function Careers() {
   useEffect(() => {
     window.location.href =
-      "https://ca.indeed.com/viewjob?cmp=Good-People-Only&t=Label&jk=2a4c218bcfab483a&q=Record+Label+Manager&vjs=3";
+      "https://ca.indeed.com/viewjob?t=executive+assistant&jk=29a804756549ca94&_gl=1*1gqy97e*_gcl_aw*R0NMLjE2NjY2NTY5NDYuQ2p3S0NBanc3OWlhQmhBSkVpd0FQWXdvQ01hVmctSExlVnRqMFNia0xWbFJ1N1VfZXJ3YXRfSUliYm5UenFUb1BhWjN5dEhZU05QelRCb0M5VllRQXZEX0J3RQ..*_gcl_dc*R0NMLjE2NjY2NTY5NDYuQ2p3S0NBanc3OWlhQmhBSkVpd0FQWXdvQ01hVmctSExlVnRqMFNia0xWbFJ1N1VfZXJ3YXRfSUliYm5UenFUb1BhWjN5dEhZU05QelRCb0M5VllRQXZEX0J3RQ..&_ga=2.228820764.12836882.1666650726-912328923.1662598037&_gac=1.250357236.1666656946.CjwKCAjw79iaBhAJEiwAPYwoCMaVg-HLeVtj0SbkLVlRu7U_erwat_IIbbnTzqToPaZ3ytHYSNPzTBoC9VYQAvD_BwE";
   });
   return true;
 }
